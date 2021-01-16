4bf04b25910ca3d2f21e98030317000149689473
Evraa <evramyousef@gmail.com>
2021-01-16 06:02:56 +0200

    all nosql qureies are updated, hoepfully for the last time

--- queries/nosql_queries.py
+++ queries/nosql_queries.py
@@ -26,26 +26,14 @@ def view_one_doc(cols):
         print (col.find_one())
 
 
-def q_1(user_obj, match_obj,team_obj):
+def q_1(user_obj, match_obj,team_obj, city, team_id):
     '''
-        1- select users from specific city that watched a specific team in the team's home match
+        (select users (with specific fname or from specific city) that watched a specific team in the team's home match).
     '''
 
-    # evs = user_obj.find({'city':city},{'_id':1})
-    # for ev in evs:
-    #     pprint.pprint(ev)
-    # return
-
-    # random_team = list(users.aggregate([{'$sample': {'size':1}}]))[0]['city']
-    # print(random_team)
-    # return
-    # for r in random_team:
-    #     pprint.pprint(r['team_name'])
-    # return
     pipeline = [
         #1- find the match_ids with team_x as home
-        # {'$match': {'teams.home':team_obj.find_one(team_id)['team_name']}},
-        {'$match': {'teams.home': list(team_obj.aggregate([{'$sample': {'size':1}}]))[0]['team_name'] } },
+        {'$match': {'teams.home': team_obj.find_one(team_id)['team_name']} },
         {'$project': {'_id':0,'users_reserved.user_id':1} },
         {'$unwind':"$users_reserved"},
         #2- unwind and group fans
@@ -58,7 +46,7 @@ def q_1(user_obj, match_obj,team_obj):
             'as':'users_info'
         }},
         #4- get fans at specific city
-        {'$match': {'users_info.city':list(users.aggregate([{'$sample': {'size':1}}]))[0]['city']}},
+        {'$match': {'users_info.city':city} },
         #5- project results
         {'$project':{'_id':0,'fname':'$users_info.fname','lname':'$users_info.lname'}}
     ]
@@ -73,17 +61,17 @@ def q_1(user_obj, match_obj,team_obj):
     print (f'Results Count: {count}')
 
 
-def q_2(user_obj,match_obj,date_l='1999-07-02T07:57:33.000000',date_u='2000-07-02T07:57:33.000000'):
+def q_2(user_obj, match_obj):
     '''
-        (select users that attended matches in range of date).
+    (select users that attended any match and sit at i.e. x = 10 and y = 12).
     '''
-
     pipeline = [
-        {'$match': {'date_time': {'$gt':date_l , '$lt': date_u}  }},
-
-        {'$project': {'_id':0, 'users_reserved.user_id':1}},
-
-        {'$unwind':"$users_reserved"},
+        {'$project': {'_id':0, 'users_reserved.y_i':1,'users_reserved.x_i':1, 'users_reserved.user_id':1}},
+        {'$unwind': '$users_reserved'},
+        {'$match': {  '$and': [ {'users_reserved.y_i': {'$eq':12}} , 
+                                {'users_reserved.x_i': {'$eq':10}}
+                            ] 
+                    }},
 
         {'$lookup':{
             'from':'users',
@@ -92,9 +80,9 @@ def q_2(user_obj,match_obj,date_l='1999-07-02T07:57:33.000000',date_u='2000-07-0
             'as':'users_info'
         }},
         {'$project':{'_id':0,'fname':'$users_info.fname','lname':'$users_info.lname'}}
-       
                     
     ]
+
     match_docs = match_obj.aggregate(pipeline)
     print ("Printing Results..")
     count = 0
@@ -103,51 +91,17 @@ def q_2(user_obj,match_obj,date_l='1999-07-02T07:57:33.000000',date_u='2000-07-0
         pprint.pprint(match_doc)
         count+=1
     print (f'Results Count: {count}')
-
 
 def q_3(user_obj, match_obj):
     '''
-    (select users that attended any match and sit on the first row i.e. y = 1).
-    '''
-
-    pipeline = [
-        {'$project': {'_id':0, 'users_reserved.y_i':1, 'users_reserved.user_id':1}},
-        {'$unwind': '$users_reserved'},
-        {'$match': { 'users_reserved.y_i': 1}},
-
-        {'$lookup':{
-            'from':'users',
-            'localField':'users_reserved.user_id',
-            'foreignField':'_id',
-            'as':'users_info'
-        }},
-        {'$project':{'_id':0,'fname':'$users_info.fname','lname':'$users_info.lname'}}
-                    
-    ]
-
-    match_docs = match_obj.aggregate(pipeline)
-    print ("Printing Results..")
-    count = 0
-    for match_doc in match_docs:
-        
-        pprint.pprint(match_doc)
-        count+=1
-    print (f'Results Count: {count}')
-
-
-
-def q_4(user_obj, match_obj):
-    '''
-    2 -> 15
-    far : x > 7 and y > 7
-    (select users that attended any match and sit far i.e. x = 20 and y = 50).
+    (select users that attended any match and sit far i.e. x > 12 and y > 12).
     '''
     
     pipeline = [
         {'$project': {'_id':0, 'users_reserved.y_i':1,'users_reserved.x_i':1, 'users_reserved.user_id':1}},
         {'$unwind': '$users_reserved'},
-        {'$match': {  '$and': [ {'users_reserved.y_i': {'$gt':7}} , 
-                                {'users_reserved.x_i': {'$gt':7}}
+        {'$match': {  '$and': [ {'users_reserved.y_i': {'$gt':10}} , 
+                                {'users_reserved.x_i': {'$gt':10}}
                             ] 
                     }},
 
@@ -171,6 +125,75 @@ def q_4(user_obj, match_obj):
     print (f'Results Count: {count}')
 
 
+def q_4(user_obj, match_obj,team_obj, fname, city, team_id):
+    '''
+        (select users (with specific fname or from specific city) that watched a specific team in the team's home match).
+    '''
+
+    pipeline = [
+        #1- find the match_ids with team_x as home
+        {'$match': {'teams.home': team_obj.find_one(team_id)['team_name']} },
+        {'$project': {'_id':0,'users_reserved.user_id':1} },
+        {'$unwind':"$users_reserved"},
+        #2- unwind and group fans
+        {'$group':{'_id':'$users_reserved.user_id'}},
+        #3- match fans with users
+        {'$lookup':{
+            'from':'users',
+            'localField':'_id',
+            'foreignField':'_id',
+            'as':'users_info'
+        }},
+        #4- get fans at specific city
+        {'$match': {'users_info.city':city} },
+        #4-2 specific fname
+        {'$match': {'users_info.fname':fname} },
+        #5- project results
+        {'$project':{'_id':0,'fname':'$users_info.fname','lname':'$users_info.lname'}}
+    ]
+
+    match_docs = match_obj.aggregate(pipeline)
+    print ("Printing Results..")
+    count = 0
+    for match_doc in match_docs:
+        
+        pprint.pprint(match_doc)
+        count+=1
+    print (f'Results Count: {count}')
+
+
+def q_5(user_obj, match_obj,team_obj, fname, city, team_id):
+    q_4(user_obj, match_obj,team_obj, fname, city, team_id)
+    
+
+def q_6(user_obj, match_obj):
+    '''
+        (select users that sit on a seat where y = 10).
+    '''
+    
+    pipeline = [
+        {'$project': {'_id':0, 'users_reserved.y_i':1,'users_reserved.x_i':1, 'users_reserved.user_id':1}},
+        {'$unwind': '$users_reserved'},
+        {'$match': { 'users_reserved.y_i': 10 }},
+
+        {'$lookup':{
+            'from':'users',
+            'localField':'users_reserved.user_id',
+            'foreignField':'_id',
+            'as':'users_info'
+        }},
+        {'$project':{'_id':0,'fname':'$users_info.fname','lname':'$users_info.lname'}}
+                    
+    ]
+
+    match_docs = match_obj.aggregate(pipeline)
+    print ("Printing Results..")
+    count = 0
+    for match_doc in match_docs:
+        
+        pprint.pprint(match_doc)
+        count+=1
+    print (f'Results Count: {count}')
 
 
 if __name__ == "__main__":
@@ -178,11 +201,17 @@ if __name__ == "__main__":
     users, matches, stadiums, teams = mydb['users'], mydb['matches'],mydb['stadiums'],mydb['teams']
     # view_one_doc([users,matches,stadiums,teams])
     
-    # q_1(users,matches,teams)
+    # q_1(users, matches, teams,city="South Jeffreybury", team_id = ObjectId('8f85a4dabefe50c1b01beace'))
     # q_2(users,matches)
     # q_3(users,matches)
-    # q_4(users, matches)
+    # q_4(users, matches, teams, fname="Kathy", 
+    #       city="South Jeffreybury", team_id = ObjectId('8f85a4dabefe50c1b01beace'))
+    # q_5(users, matches, teams, fname="Kathy", 
+    #       city="South Jeffreybury", team_id = ObjectId('8f85a4dabefe50c1b01beace'))
     
+    # q_6(users,matches)
+    
+
 
     # first_five = teams.find().limit(50)
     # for f in first_five:
